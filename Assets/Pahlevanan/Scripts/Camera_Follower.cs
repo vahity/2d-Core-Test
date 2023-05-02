9174f1c5cc1b45311cfbcb67851dc4728211e12e
vahid <vahidgamer11@gmail.com>
2023-05-02 10:34:03 +0430

    hichi1

--- Assets/Pahlevanan/Scripts/Camera_Follower.cs
+++ Assets/Pahlevanan/Scripts/Camera_Follower.cs
@@ -6,7 +6,12 @@ public class Camera_Follower : MonoBehaviour
 {
     public static float FollowSpeed = 0.01f;
     [SerializeField] private float yOffset = 0f;
+    [SerializeField] private float xStartPos = 0f;
     public Transform target;
+     void Start()
+    {
+        Vector3 newPos = new Vector3(xStartPos, yOffset, -10f);
+    }
     void Update()
     {
        Vector3 newPos = new Vector3(target.position.x+4,yOffset, -10f); 
